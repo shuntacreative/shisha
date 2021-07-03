2ac763225c6536e568d71fd0c41e73f21c5d3eaf
shuntacreative <shuntacreative@gmail.com>
2021-07-03 17:42:15 +0900

    写真投稿機能

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,24 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
+def new
+  @photo = current_user.photos.new
+end
+
+def create
+  @photo = current_user.photos.new(photo_params)
+
+  if @photo.save
+    redirect_to :root
+  else
+    render :new
+  end
+end
+
+private_methods
+
+def photo_params
+  params.require(:photo).permit(:title, :image)
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ has_many :photos
+  
 
    with_options presence: true do
      validates :birthday
